0485c6bddb78c97fc4bc88271d638cdef5456861
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-03-24 17:57:46 -0700

    Moved callbacks to their own header/cpp

--- include/KenshiPy_Runtime.h
+++ include/KenshiPy_Runtime.h
@@ -9,6 +9,3 @@ void TryLoadMods();
 
 __declspec(dllexport) void RunScript(const std::string& scriptPath);
 __declspec(dllexport) void RunString(const std::string& code);
-
-void RegisterKeyDownCallback(PyObject* callable);
-void CallKeyDownCallbacks(int keyCode);
