1008c46f402e70ba0b4432d713a79b236868f887
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-03-24 17:47:03 -0700

    Don't remember why I had this file, and it doesn't seem to be necessary anymore

--- include/ArtifactItemData_stub.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// Minimal stub for ArtifactItemData to satisfy Ogre::FastArray<ArtifactItemData>
-// instantiation in Town.h. The full definition is in the game binary.
-class ArtifactItemData
-{
-    ArtifactItemData() {}
-    ArtifactItemData(const ArtifactItemData&) {}
-    ArtifactItemData& operator=(const ArtifactItemData&) { return *this; }
-};
