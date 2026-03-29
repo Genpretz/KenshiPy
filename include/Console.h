7bf41b85c0e391f5876d0a0d223e921c33badd3e
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-03-28 23:08:15 -0700

    Added functions/variables from Consoole.cpp to the header

--- include/Console.h
+++ include/Console.h
@@ -4,7 +4,11 @@
 
 namespace Console
 {
+    extern bool g_consoleInitialized;
+
     void Init();
     void Toggle();
     void AppendOutput(const std::string& text);
+    void InitFrameHandler(float timeDelta);
+    bool IsInitialized();
 }
