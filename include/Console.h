4616fad0234502c50f76eea57cca59aeea44aac3
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-03-28 23:08:42 -0700

    Revert "Added functions/variables from Consoole.cpp to the header"
    This reverts commit 7bf41b85c0e391f5876d0a0d223e921c33badd3e.

--- include/Console.h
+++ include/Console.h
@@ -4,11 +4,7 @@
 
 namespace Console
 {
-    extern bool g_consoleInitialized;
-
     void Init();
     void Toggle();
     void AppendOutput(const std::string& text);
-    void InitFrameHandler(float timeDelta);
-    bool IsInitialized();
 }
