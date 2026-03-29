79d898df54071c85848320a966933a14182acc4b
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-03-28 23:00:50 -0700

    Moved hooks to a new seperate file

---
+++ src/Hooks.cpp
@@ -0,0 +1,61 @@
+#include "Hooks.h"
+
+#include "Callbacks.h"
+#include "Console.h"
+#include "KenshiPy_Runtime.h"
+
+#include <Debug.h>
+#include <core/Functions.h>
+#include <kenshi/gui/TitleScreen.h>
+#include <kenshi/InputHandler.h>
+#include <kenshi/Globals.h>
+
+#include "mygui/MyGUI_Gui.h"
+#include "mygui/MyGUI_Widget.h"
+
+void (*InputHandler_keyDownEvent_orig)(InputHandler*, OIS::KeyCode) = NULL;
+void InputHandler_keyDownEvent_hook(InputHandler* thisptr, OIS::KeyCode keyCode)
+{
+    if (InputHandler_keyDownEvent_orig)
+        InputHandler_keyDownEvent_orig(thisptr, keyCode);
+    if (keyCode == OIS::KC_GRAVE && thisptr->alt)
+        Console::Toggle();
+    CallKeyDownCallbacks((int)keyCode);
+}
+
+TitleScreen* (*TitleScreen_orig)(TitleScreen*) = NULL;
+TitleScreen* TitleScreen_hook(TitleScreen* thisptr)
+{
+    TitleScreen* result = TitleScreen_orig(thisptr);
+    TryLoadMods();
+
+    if (!Console::IsInitialized())
+    {
+        MyGUI::Gui* gui = MyGUI::Gui::getInstancePtr();
+        if (gui)
+            gui->eventFrameStart += MyGUI::newDelegate(Console::InitFrameHandler);
+    }
+
+    return result;
+}
+
+bool installInputHandlerHooks()
+{
+    if (KenshiLib::SUCCESS != KenshiLib::AddHook(KenshiLib::GetRealAddress(&InputHandler::keyDownEvent), InputHandler_keyDownEvent_hook, &InputHandler_keyDownEvent_orig))
+    {
+        ErrorLog("Failed to hook InputHandler::keyDownEvent.");
+        return false;
+    }
+    return true;
+}
+
+bool installTitlescreenHooks()
+{
+    if (KenshiLib::SUCCESS != KenshiLib::AddHook(KenshiLib::GetRealAddress(&TitleScreen::_CONSTRUCTOR), TitleScreen_hook, &TitleScreen_orig))
+    {
+        ErrorLog("Failed to hook Titlescreen::_CONSTRUCTOR.");
+        return false;
+    }
+
+    return true;
+}
