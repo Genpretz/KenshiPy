bdf122ff5174c4b464f04c577dc1d4bff5d2e186
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-06-12 04:39:35 -0700

    Remove the logging calls from the script

--- example scripts/GoProne.py
+++ example scripts/GoProne.py
@@ -2,45 +2,26 @@ import KenshiPy
 
 KC_V = 47
 
-def _toggle_prone():
-    KenshiPy.DebugLog("[Go Prone] _toggle_prone called")
-
+def toggle_prone():
     world = KenshiPy.getGameWorld()
-    KenshiPy.DebugLog("[Go Prone] Got world: {}".format(world))
 
     if not world or not world.player:
-        KenshiPy.DebugLog("[Go Prone] No world or no player")
         return
-
     c = world.player.selectedCharacter.getCharacter()
-    KenshiPy.DebugLog("[Go Prone] Got character: {}".format(c))
-
     if not c:
-        KenshiPy.ErrorLog("[Go Prone] No selected character.")
         return
 
     state = c.getProneState()
-    KenshiPy.DebugLog("[Go Prone] Current prone state: {}".format(state))
-
     if state == KenshiPy.PS_NORMAL:
-        KenshiPy.DebugLog("[Go Prone] Switching to STAYING_LOW")
         c.setStealthMode(True)
         c.setProneState(KenshiPy.PS_STAYING_LOW)
     elif state == KenshiPy.PS_STAYING_LOW:
-        KenshiPy.DebugLog("[Go Prone] Switching to NORMAL")
         c.setProneState(KenshiPy.PS_NORMAL)
         c.setStealthMode(False)
-    else:
-        KenshiPy.DebugLog("[Go Prone] Unknown state: {}".format(state))
-        return
 
 def on_key_down(key_code):
-    KenshiPy.DebugLog("[Go Prone] Key pressed: {}".format(key_code))
     if key_code == KC_V:
-        KenshiPy.DebugLog("[Go Prone] V key detected, toggling prone")
-        _toggle_prone()
-
-KenshiPy.DebugLog("[Go Prone] Registering key callback")
+        toggle_prone()
 
 # RegisterKeyDownCallback expects a function that takes a single argument
 KenshiPy.RegisterKeyDownCallback(on_key_down)
